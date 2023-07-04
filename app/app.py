10c3a95a0fa028d9b985ff525fca2ae3f9e63329
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-07-04 12:43:04 +0300

    migrations done

---
+++ app/app.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource, reqparse
+from models import Guest, db, Episode, Appearance
+
+from models import db, Episode
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+api = Api(app)
+
+
+class EpisodeResource(Resource):
+    def get(self):
+        episodes = Episode.query.all()
+        data = [
+            {
+                'id': episode.id,
+                'date': episode.date,
+                'number': episode.number
+            }
+            for episode in episodes
+        ]
+        return data
+
+
+class SingleEpisodeResource(Resource):
+    def get(self, id):
+        episode = Episode.query.get(id)
+        if episode:
+            data = {
+                'id': episode.id,
+                'date': episode.date,
+                'number': episode.number,
+                'guests': [
+                    {
+                        'id': appearance.guest.id,
+                        'name': appearance.guest.name,
+                        'occupation': appearance.guest.occupation
+                    }
+                    for appearance in episode.appearances
+                ]
+            }
+            return data
+        else:
+            return {'error': 'Episode not found'}, 404
+
+    def delete(self, id):
+        episode = Episode.query.get(id)
+        if episode:
+            db.session.delete(episode)
+            db.session.commit()
+            return '', 204
+        else:
+            return {'error': 'Episode not found'}, 404
+
+
+class GuestResource(Resource):
+    def get(self):
+        guests = Guest.query.all()
+        data = [
+            {
+                'id': guest.id,
+                'name': guest.name,
+                'occupation': guest.occupation
+            }
+            for guest in guests
+        ]
+        return data
+
+
+class AppearanceResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('rating', type=int, required=True)
+    parser.add_argument('episode_id', type=int, required=True)
+    parser.add_argument('guest_id', type=int, required=True)
+
+    def post(self):
+        args = self.parser.parse_args()
+        rating = args['rating']
+        episode_id = args['episode_id']
+        guest_id = args['guest_id']
+
+        episode = Episode.query.get(episode_id)
+        guest = Guest.query.get(guest_id)
+
+        if not episode:
+            return {'error': 'Episode not found'}, 404
+
+        if not guest:
+            return {'error': 'Guest not found'}, 404
+
+        if not (1 <= rating <= 5):
+            return {'errors': ['Validation error. Rating must be between 1 and 5 (inclusive).']}, 400
+
+        appearance = Appearance(rating=rating, episode=episode, guest=guest)
+        db.session.add(appearance)
+        db.session.commit()
+
+        response_data = {
+            'id': appearance.id,
+            'rating': appearance.rating,
+            'episode': {
+                'id': episode.id,
+                'date': episode.date,
+                'number': episode.number
+            },
+            'guest': {
+                'id': guest.id,
+                'name': guest.name,
+                'occupation': guest.occupation
+            }
+        }
+        return response_data, 201
+
+
+api.add_resource(EpisodeResource, '/episodes')
+api.add_resource(SingleEpisodeResource, '/episodes/<int:id>')
+api.add_resource(GuestResource, '/guests')
+api.add_resource(AppearanceResource, '/appearances')
+
+
+@app.route('/')
+def home():
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
---
+++ app/models.py
@@ -0,0 +1,47 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Episode(db.Model):
+    __tablename__ = 'Episode'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship(
+        'Appearance', backref='episode', cascade='all, delete-orphan')
+
+
+class Appearance(db.Model):
+    __tablename__ = 'appearance'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey(
+        'episode.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guest.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validate('rating')
+    def validate_rating(self, key, rating):
+        if not (1 <= rating <= 5):
+            raise ValueError("Rating must be between 1 and 5 (inclusive).")
+        return rating
+
+
+class Guest(db.Model):
+    __tablename__ = 'guest'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    occupation = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship(
+        'Appearance', backref='guest', cascade='all, delete-orphan')
